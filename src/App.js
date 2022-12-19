61bf6c54b16d98e50de750f1016e2a356354f551
Giraud Julemis <giraud.julemis@gmail.com>
2022-12-19 17:18:21 -0500

    build rank and image link form component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import Navigation from "./component/Navigation/Navigation";
 import Logo from "./component/Logo/Logo";
 import "./App.css";
+import ImageLinkForm from "./component/ImageLinkForm/ImageLinkForm";
+import Rank from "./component/Rank/Rank";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
+      <ImageLinkForm />
     </div>
   );
 }
